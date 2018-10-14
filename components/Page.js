9660370d95a759418db798de6f37ddcd017e9eff
Andreas Sander <andreas.sander@hotmail.de>
2018-10-14 21:00:31 +0200

    Cleanup demo content, replace by more realistic stuff.

--- components/Page.js
+++ components/Page.js
@@ -1,36 +1,7 @@
 import React, { Component } from "react";
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
-
-const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
-      id
-      title
-      description
-    }
-  }
-`;
 class Page extends Component {
   render() {
-    return (
-      <main>
-        {this.props.children}
-        <Query query={ALL_ITEMS_QUERY}>
-          {({ data, error, loading }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error: {error.message}</p>;
-            return (
-              <ul>
-                {data.items.map((item, index) => (
-                  <li key={index}>{item.title}</li>
-                ))}
-              </ul>
-            );
-          }}
-        </Query>
-      </main>
-    );
+    return <main>{this.props.children}</main>;
   }
 }
 
