14f53b36fa4e9529e447bb8d22ee35ecfce98bda
Andreas Sander <andreas.sander@hotmail.de>
2018-10-26 21:52:13 +0200

    MVP Create Event

--- components/CreateEvent.js
+++ components/CreateEvent.js
@@ -3,18 +3,32 @@ import PropTypes from "prop-types";
 import { Pane, Checkbox, TextInput, Button, RadioGroup } from "evergreen-ui";
 import { Mutation, Query } from "react-apollo";
 import { CREATE_EVENT_MUTATION } from "../lib/mutations/events";
+import { ALL_SERIES_QUERY } from "../lib/queries/eventSeries";
 import { ALL_PARTICIPANTS_QUERY } from "../lib/queries/participants";
+import DatePicker from "react-datepicker";
+import moment from "moment";
+
+// Import date-picker css
+import("react-datepicker/dist/react-datepicker.css");
 class CreateEvent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "",
-      date: null,
+      title: "a test",
+      date: moment(),
+      series: {},
       seriesNumber: 1,
       location: null,
-      series: null,
       participants: []
     };
+
+    this.onChangeDate = this.onChangeDate.bind(this);
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date
+    });
   }
 
   render() {
@@ -31,13 +45,55 @@ class CreateEvent extends React.Component {
             >
               <h2>Create an Event</h2>
               <fieldset disabled={loading} aria-busy={loading}>
+                <TextInput
+                  placeholder="Titel"
+                  value={this.state.title}
+                  required={true}
+                  onChange={e => this.setState({ title: e.target.value })}
+                />
                 <input
                   type="number"
                   placeholder="Wievieltes Treffen"
                   value={this.state.seriesNumber}
-                  onChange={e => this.setState({ seriesNumber: e.target.value })}
+                  onChange={e =>
+                    this.setState({ seriesNumber: e.target.value })
+                  }
                   min="1"
                 />
+
+                <label>When</label>
+                <DatePicker
+                  selected={this.state.date}
+                  onChange={this.onChangeDate}
+                />
+
+                <h1>Choose serie</h1>
+                <Query query={ALL_SERIES_QUERY}>
+                  {({ data, error, loading }) => {
+                    console.log(data, error, loading);
+                    if (error || loading) {
+                      return <h1>Nope</h1>;
+                    }
+
+                    return (
+                      <Pane>
+                        <RadioGroup
+                          label="Serie"
+                          value={this.state.contactType}
+                          options={data.eventSeries.map(serie => ({
+                            label: serie.title,
+                            value: serie.id
+                          }))}
+                          onChange={value =>
+                            this.setState({
+                              series: { connect: { id: value } }
+                            })
+                          }
+                        />
+                      </Pane>
+                    );
+                  }}
+                </Query>
 
                 <Fragment>
                   <h1>Choose some participants</h1>
